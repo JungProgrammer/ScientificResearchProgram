ad0c1f21e23b5e82617e2f653891b6cb29e1784a
JungProgrammer <artyom.najdanov@mail.ru>
2021-02-02 12:48:38 +0500

    Конец первой версии (так думаем)

--- ResearchProgramWPF/ResearchProgram/Forms/FullGrantInfo.xaml.cs
+++ ResearchProgramWPF/ResearchProgram/Forms/FullGrantInfo.xaml.cs
@@ -22,67 +22,86 @@ namespace ResearchProgram.Forms
     /// </summary>
     public partial class FullGrantInfo : Window
     {
-        public class DepositorSum: INotifyPropertyChanged
+        public class DepositorSum
         {
-            private float _sum;
-            public float sum
-            {
-                get => _sum;
-                set
-                {
-                    _sum = value;
-                    OnPropertyChanged(nameof(sum));
-                }
-            }
+            public string Sum { get; set; }
+        }
 
-            public override string ToString()
-            {
-                return sum.ToString();
-            }
-
-
-            public event PropertyChangedEventHandler PropertyChanged;
-            public void OnPropertyChanged([CallerMemberName] string prop = "")
-            {
-                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
-            }
+        public class RecieptDate
+        {
+            public string Date { get; set; }
         }
 
         public ObservableCollection<DepositorSum> depositorSums;
         public ObservableCollection<DepositorSum> depositorSumsNoNDS;
+
+        public ObservableCollection<RecieptDate> recieptDates;
 
 
         public FullGrantInfo(Grant grant)
         {
             InitializeComponent();
 
+            SetStartViewModelSettings(grant);
+
             CopyDepositors(grant);
+            CopyRecieptDates(grant);
 
             DataContext = grant;
 
-            SetDataContextDepositors();
+            SetDataContexts();
         }
 
+        private void SetStartViewModelSettings(Grant grant)
+        {
+            grant.Depositor.Insert(0, new Depositor() { Title = "Источники:" });
+        }
+
+        private void CopyRecieptDates(Grant grant)
+        {
+            recieptDates = new ObservableCollection<RecieptDate>();
+
+            recieptDates.Add(new RecieptDate() { Date = "Даты поступления:" });
+
+            foreach(string recieptDate in grant.ReceiptDate)
+            {
+                recieptDates.Add(new RecieptDate() { Date = recieptDate });
+            }
+        }
 
         private void CopyDepositors(Grant grant)
         {
             depositorSums = new ObservableCollection<DepositorSum>();
             depositorSumsNoNDS = new ObservableCollection<DepositorSum>();
 
+            depositorSums.Add(new DepositorSum() { Sum = "Сумма:" });
+            depositorSumsNoNDS.Add(new DepositorSum() { Sum = "Сумма:" });
+
             foreach (float depositorSum in grant.DepositorSum)
             {
-                depositorSums.Add(new DepositorSum() { sum = depositorSum });
+                depositorSums.Add(new DepositorSum() { Sum = depositorSum.ToString() });
             }
 
             foreach (float depositorSumNoNDS in grant.DepositorSumNoNDS)
             {
-                depositorSumsNoNDS.Add(new DepositorSum() { sum = depositorSumNoNDS });
+                depositorSumsNoNDS.Add(new DepositorSum() { Sum = depositorSumNoNDS.ToString() });
             }
         }
 
-        private void SetDataContextDepositors()
+        private void SetDataContexts()
         {
-            DepositorsSumsControl.ItemsSource = depositorSums;
+            // Установка привзяки для сумм
+            if (Settings.Default.NDSKey)
+            {
+                DepositorsSumsControl.ItemsSource = depositorSums;
+            }
+            else
+            {
+                DepositorsSumsControl.ItemsSource = depositorSumsNoNDS;
+            }
+
+            // Установка привязки для дат
+            RecieptDatesControl.ItemsSource = recieptDates;
         }
 
     }
