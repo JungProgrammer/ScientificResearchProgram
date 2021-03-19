321fa2aba2e685cf583a360dbdea4f517be01356
TrueSeeker322 <murz.kirill@yandex.ru>
2021-03-19 12:56:20 +0500

    Изменение версии

--- ResearchProgramWPF/ResearchProgram/Settings.Designer.cs
+++ ResearchProgramWPF/ResearchProgram/Settings.Designer.cs
@@ -133,7 +133,7 @@ namespace ResearchProgram {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("1.32")]
+        [global::System.Configuration.DefaultSettingValueAttribute("1.33")]
         public string ProgrammVersion {
             get {
                 return ((string)(this["ProgrammVersion"]));
